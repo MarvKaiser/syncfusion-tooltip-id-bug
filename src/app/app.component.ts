ee710f06830bb5c7351e6a4c3b785fff6717a7a6
Marvin Kaiser <marvin.kaiser@vector.com>
2023-08-21 16:59:06 +0200

    proofed, if id is just numeric, the tooltip does not work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AxisModel} from "@syncfusion/ej2-charts/src/chart/axis/axis-model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'syncfusion-bar-chart-tooltip';
+  public chartData!: Object[];
+  public primaryXAxis!: AxisModel;
+  public primaryYAxis!: AxisModel;
+  public tooltip!: any;
+  public marker!: any;
+
+  ngOnInit() {
+    this.chartData = [
+      { month: 'Jan', sales: 35 }, { month: 'Feb', sales: 28 },
+      { month: 'Mar', sales: 34 }, { month: 'Apr', sales: 32 },
+      { month: 'May', sales: 40 }, { month: 'Jun', sales: 32 },
+      { month: 'Jul', sales: 35 }, { month: 'Aug', sales: 55 },
+      { month: 'Sep', sales: 38 }, { month: 'Oct', sales: 30 },
+      { month: 'Nov', sales: 25 }, { month: 'Dec', sales: 32 }
+    ];
+
+    this.primaryXAxis = {
+      valueType: 'Category'
+    };
+
+    this.primaryYAxis = {
+      labelFormat: '${value}K'
+    };
+
+    this.tooltip = {
+      enable: true
+    }
+
+    this.marker = {
+        visible: true
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {
+  BarSeriesService,
+  CategoryService,
+  ChartModule, DataLabelService,
+  LegendService,
+  LineSeriesService,
+  TooltipService
+} from "@syncfusion/ej2-angular-charts";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ChartModule
   ],
-  providers: [],
+  providers: [CategoryService, LegendService, TooltipService, DataLabelService, LineSeriesService, BarSeriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
